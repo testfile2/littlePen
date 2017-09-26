1de45404e78ae6d3945129ec082b5506ad5cebd8
Keegan COWLE <kcowle@student.42.fr>
2017-09-26 10:16:33 +0200

    push

--- ex07/main.c
+++ ex07/main.c
@@ -6,6 +6,12 @@
 #include <linux/fs.h>
 #include <linux/uaccess.h>
 #include <linux/debugfs.h>
+#include <linux/timer.h>
+
+
+#define BUF_SIZE 4096
+static char foo_buf[BUF_SIZE];
+
 
 static struct dentry *my_debugfs_root;
 
@@ -46,7 +52,7 @@ static ssize_t main_read(struct file *flip,
 {
 	char *login = "kcowle";
 	int index = 0;
-	
+
 	if (buf[0] == 'k')
 	{
 		put_user('\0', buf);
@@ -70,11 +76,49 @@ static const struct file_operations sample_fops = {
     .llseek 			= no_llseek,
 };
 
+static ssize_t jiffy_read(struct file *flip,
+			char *buf, size_t count, loff_t *offset)
+{
+    char jiff[10];
+    sprintf(jiff, "%ul", jiffies);
+    return simple_read_from_buffer(buf, count, offset, jiff, 10);
+}
+
+static const struct file_operations jiffie_fops = {
+    .read		 	    = jiffy_read,
+};
+
+static ssize_t foo_read(struct file *flip,
+			char *buf, size_t count, loff_t *offset)
+{
+
+}
+
+static ssize_t foo_write(struct file *file, const char *buf,
+                            size_t len, loff_t *ppos)
+{
+    size_t ret;
+
+    if (*ppos > BUF_SIZE)
+        return -EINVAL;
+    ret = simple_write_to_buffer(foo_buf, sizeof(foo_buf), ppos, buf, len);
+    if (ret < 0)
+        return ret;
+    foo_buf[ret] = '\0';
+
+    return ret;
+}
+
+static const struct file_operations foo_fops = {
+    .read		 	    = foo_read,
+    .write              = foo_write,
+};
+
 static int __init misc_init(void)
 {
 	my_debugfs_root = debugfs_create_dir("fortytwo", NULL);
 	debugfs_create_file("id", 0666, my_debugfs_root, NULL, &sample_fops);
-	
+    debugfs_create_file("jiffies", 0644, my_debugfs_root, NULL, &jiffie_fops);
 
 	return 0;
 }
