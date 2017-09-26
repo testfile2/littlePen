9b04afb6eec5cdda7bf20a29c4974db44721d887
Keang Nage <Keyoung-n@users.noreply.github.com>
2017-09-26 13:33:07 +0200

    Update 42_module.c

--- ex08/42_module.c
+++ ex08/42_module.c
@@ -9,21 +9,25 @@ MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Louis Solofrizzo <louis@ne02ptzero.me>");
 MODULE_DESCRIPTION("Useless module");
 
-static ssize_t myfd_read(struct file *fp, char __user *user, size_t size, loff_t *offs);
+static ssize_t myfd_read(struct file *fp,
+                         char __user *user,
+                         size_t size, loff_t *offs);
 
-static ssize_t myfd_write(struct file *fp, const char __user *user, size_t size, loff_t *offs);
+static ssize_t myfd_write(struct file *fp,
+                          const char __user *user,
+                          size_t size,
+                          loff_t *offs);
 
 static struct file_operations myfd_fops = {
 	.owner = THIS_MODULE,
-    .read = myfd_read,
-    .write = myfd_write,
+	.read  = myfd_read,
+	.write = myfd_write,
 };
 
-static struct miscdevice myfd_device =
-{
+static struct miscdevice myfd_device = {
 	.minor = MISC_DYNAMIC_MINOR,
-	.name = "reverse",
-	.fops = &myfd_fops,
+	.name  = "reverse",
+	.fops  = &myfd_fops,
 };
 
 char str[PAGE_SIZE];
@@ -31,13 +35,12 @@ char tmp[PAGE_SIZE];
 
 static int __init myfd_init(void)
 {
-  if (misc_register(&myfd_device))
-    printk(KERN_ERR "Unable register misc device");
+	if (misc_register(&myfd_device))
+		printk(KERN_ERR "Unable register misc device");
 	return 0;
 }
 
-static void __exit myfd_cleanup(void)
-{
+static void __exit myfd_cleanup(void) {
 	misc_deregister(&myfd_device);
 }
 
@@ -45,19 +48,20 @@ ssize_t myfd_read(struct file *fp, char __user *user, size_t size, loff_t *offs)
 {
 	size_t t, i;
 
-	for (t = strlen(str) - 1, i = 0; t != -1; t--, i++)
-	{
+	for (t = strlen(str) - 1, i = 0; t != -1; t--, i++) {
 		tmp[i] = str[t];
 	}
 	tmp[i] = 0x0;
-	return simple_read_from_buffer(user, size, offs, tmp, i);
+	return simple_read_from_buffer(user, size, offs, tmp, i) + 1;
 }
 
-ssize_t myfd_write(struct file *fp, const char __user *user, size_t size, loff_t *offs)
+ssize_t myfd_write(struct file *fp,
+                   const char __user *user,
+                   size_t size,
+                   loff_t *offs)
 {
-	ssize_t res;
-	res = 0;
-	res = simple_write_to_buffer(str, size, offs, user, size);
+	ssize_t res = 0;
+	res = simple_write_to_buffer(str, size, offs, user, size) + 1;
 	str[size + 1] = 0x0;
 	return res;
 }
