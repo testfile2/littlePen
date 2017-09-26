6bd27fd40c2c2fbdfebec86f4f5ee5304b86be17
Keegan COWLE <kcowle@student.42.fr>
2017-09-26 10:21:48 +0200

    LOL

--- ex08/42_module.c
+++ ex08/42_module.c
@@ -4,8 +4,8 @@
 #include <linux/miscdevice.h>
 #include <linux/fs.h>
 #include <linux/slab.h>
-// Dont have a license, LOL
-MODULE_LICENSE("LICENSE");
+
+MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Louis Solofrizzo <louis@ne02ptzero.me>");
 MODULE_DESCRIPTION("Useless module");
 
@@ -15,26 +15,30 @@ static ssize_t myfd_write(struct file *fp, const char __user *user, size_t size,
 
 static struct file_operations myfd_fops = {
 	.owner = THIS_MODULE,
-  .read = &myfd_read,
-  .write = &myfd_write
+    .read = myfd_read,
+    .write = myfd_write,
 };
 
-static struct miscdevice myfd_device = {
-	.minor = MISC_DYNAMIC_MINOR,.name = "reverse",
-	.fops = &myfd_fops
+static struct miscdevice myfd_device =
+{
+	.minor = MISC_DYNAMIC_MINOR,
+	.name = "reverse",
+	.fops = myfd_fops,
 };
 
 char str[PAGE_SIZE];
 char *tmp;
 
-static int __init myfd_init(void) {
+static int __init myfd_init(void)
+{
   if (misc_register(&myfd_device))
     printk(KERN_ERR "Unable register misc device");
 	return 0;
 }
 
-static void __exit myfd_cleanup(void) {
-  misc_deregister(&myfd_device);
+static void __exit myfd_cleanup(void)
+{
+	misc_deregister(&myfd_device);
 }
 
 ssize_t myfd_read(struct file *fp, char __user *user, size_t size, loff_t *offs)
@@ -51,12 +55,14 @@ ssize_t myfd_read(struct file *fp, char __user *user, size_t size, loff_t *offs)
 	return simple_read_from_buffer(user, size, offs, tmp, i);
 }
 
-ssize_t myfd_write(struct file *fp, const char __user *user, size_t size, loff_t *offs) {
+ssize_t myfd_write(struct file *fp, const char __user *user, size_t size, loff_t *offs)
+{
 	ssize_t res;
 	res = 0;
 	res = simple_write_to_buffer(str, size, offs, user, size) + 1;
 	str[size + 1] = 0x0;
 	return res;
 }
+
 module_init(myfd_init);
 module_exit(myfd_cleanup);
